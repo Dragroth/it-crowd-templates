90261234230b47fbc1a971bb5c305694fc80a2e9
Dragroth <werander.k@gmail.com>
2020-09-10 16:59:09 +0200

    NewYork - rozwój

--- NewYork/js/script.js
+++ NewYork/js/script.js
@@ -1,5 +1,5 @@
 window.onscroll = function () { 
-    var myNav = document.querySelector(".navbar");
+    var myNav = document.querySelector(".navbar-bar");
     if (window.scrollY > 50 ) {
         myNav.classList.add("navbar-other");
         myNav.classList.remove("navbar-top");
@@ -9,3 +9,16 @@ window.onscroll = function () {
         myNav.classList.remove("navbar-other");
     }
 };
+
+const menuToggler = document.querySelector(".menu-toggler");
+const navMenu = document.querySelector(".navbar-right");
+
+menuToggler.addEventListener("click", () => {
+	if(menuToggler.classList.contains("active")) {
+		menuToggler.classList.remove("active");
+		navMenu.classList.remove("active");
+	} else {
+		menuToggler.classList.add("active");
+		navMenu.classList.add("active");
+	}
+});
